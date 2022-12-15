eac32d9f50f85ebde8092fd58e0ed5d27ec4c3c8
khaitbek <khaitbek2005@gmail.com>
2022-12-15 08:06:30 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Header } from "./Components/header/Header";
+import TaskBar from "./Components/taskbar/TaskBar";
 
 function App() {
   return (
     <div className="app">
-
+      <TaskBar />
+      <Header />
     </div>
   );
 }
---
+++ src/Components/taskbar/TaskBar.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./taskBar.scss";
+import Task from '../tasks/Tasks';
+import { Overview, Tickets, Ideas, Contacts, Agents, Articles, Settings, Subscription, Dashboard } from '../../Assets/images/icons/Icons';
+const TaskBar = () => {
+    const tasks = [
+        {
+            Svg:Overview,
+            text:"Overview"
+        },
+
+        {
+            Svg:Tickets,
+            text:"Tickets"
+        },
+        {
+            Svg:Ideas,
+            text:"Ideas"
+        },
+        {
+            Svg:Contacts,
+            text:"Contacts"
+        },
+        {
+            Svg:Agents,
+            text:"Agents"
+        },
+        {
+            Svg:Articles,
+            text:"Articles"
+        },
+        {
+            Svg:Settings,
+            text:"Settings"
+        },
+        {
+            Svg:Subscription,
+            text:"Subscription"
+        },
+    ]
+    return (
+        <div className="sidebar">
+            <div className="sidebar-inner">
+                <Dashboard className="sidebar-logo" />
+                <h2 className='sidebar-title'>Dashboard Kit</h2>
+            </div>
+            <ul className="sidebar-list">
+                {
+                    tasks.map(task => <Task Svg={task.Svg} text={task.text}/>)
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default TaskBar
